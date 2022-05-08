0c56de10b238fb3b75fdb4b1c8cea0cba17e6140
cadfrunze <78301491+cadfrunze@users.noreply.github.com>
2022-05-08 15:32:24 +0300

    Update jocul.py

--- jocul.py
+++ jocul.py
@@ -4,28 +4,29 @@ from useri import useri_dict
 
 
     
-
+new_useri_dict = useri_dict
 def joc():
     rand3 = ["[_]", "[_]", "[_]"]
     rand2 = ["[_]", "[_]", "[_]"]
     rand1 = ["[_]", "[_]", "[_]"]
     tabla = [rand1, rand2, rand3]
     print(f"3 > {rand3}\n2 > {rand2}\n1 > {rand1}\n       ^      ^      ^\n       A      B      C")
-    
-    xul = input(f"{useri_dict['user_X'][0]} introdu' coordonatele (Se incepe cu randurile, respectiv 1, 2 sau 3 si apoi coloanele - A, B sau C >>> ").lower()
-    if xul[1] == "a":
-        sec_digit = 0
-    elif xul[1] == "b":
-        sec_digit = 1
-    elif xul[1] == "c":
-        sec_digit = 2
-    first_digit = xul[0]
-    first_digit = int(first_digit)
+    game1 = True
+    while game1:
+        xul = input(f"{useri_dict['user_X'][0]} introdu' coordonatele (Se incepe cu randurile, respectiv 1, 2 sau 3 si apoi coloanele - A, B sau C >>> ").lower()
+        if xul[1] == "a":
+            sec_digit = 0
+        elif xul[1] == "b":
+            sec_digit = 1
+        elif xul[1] == "c":
+            sec_digit = 2
+        first_digit = xul[0]
+        first_digit = int(first_digit)
 
-    tabla = tabla[first_digit - 1]
-    tabla[sec_digit] = len(tabla) - sec_digit
-    tabla[sec_digit] = "[x]"
-    print(f"3 > {rand3}\n2 > {rand2}\n1 > {rand1}\n       ^      ^      ^\n       A      B      C")
+        tabla = tabla[first_digit - 1]
+        tabla[sec_digit] = len(tabla) - sec_digit
+        tabla[sec_digit] = "[x]"
+        print(f"3 > {rand3}\n2 > {rand2}\n1 > {rand1}\n       ^      ^      ^\n       A      B      C")
 
 
 
