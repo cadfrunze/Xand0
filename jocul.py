749e4a7eb6e3bdb5f9f17ff3dd2a89ef3977294f
cadfrunze <78301491+cadfrunze@users.noreply.github.com>
2022-05-08 10:48:47 +0300

    Update jocul.py

--- jocul.py
+++ jocul.py
@@ -1,15 +1,19 @@
-import useri
+# import useri
 # Creeare si rulare joc
-# rand3 = ["[_]", "[_]", "[_]"]
-# rand2 = ["[_]", "[_]", "[_]"]
-# rand1 = ["[_]", "[_]", "[_]"]
-# tabla = [rand1, rand2, rand3]
-# print(f"3 > {rand3}\n2 > {rand2}\n1 > {rand1}\n       ^      ^      ^\n       A      B      C")
+
 
 
     
 
-def joc(tabla):
+def joc():
+    first = 0
+    if first == 0:
+        rand3 = ["[_]", "[_]", "[_]"]
+        rand2 = ["[_]", "[_]", "[_]"]
+        rand1 = ["[_]", "[_]", "[_]"]
+        tabla = [rand1, rand2, rand3]
+        print(f"3 > {rand3}\n2 > {rand2}\n1 > {rand1}\n       ^      ^      ^\n       A      B      C")
+    first = 1
 
     user1 = input("Introdu coordonatele(Se incepe cu randurile, respectiv 1, 2 sau 3 si apoi coloanele - A, B sau C >>> ").lower()
     if user1[1] == "a":
@@ -24,9 +28,10 @@ def joc(tabla):
     tabla = tabla[first_digit - 1]
     tabla[sec_digit] = len(tabla) - sec_digit
     tabla[sec_digit] = "[x]"
+    print(f"3 > {rand3}\n2 > {rand2}\n1 > {rand1}\n       ^      ^      ^\n       A      B      C")
     
-joc(tabla)
+joc()
 
 
 
-print(f"3 > {rand3}\n2 > {rand2}\n1 > {rand1}\n       ^      ^      ^\n       A      B      C")
+
