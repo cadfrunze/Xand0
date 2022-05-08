362c017c39208197c7203de5bf38ea4ba490ad84
cadfrunze <78301491+cadfrunze@users.noreply.github.com>
2022-05-08 13:25:20 +0300

    Update jocul.py

--- jocul.py
+++ jocul.py
@@ -1,4 +1,4 @@
-from useri import user_dict
+from useri import useri_dict
 # Creeare si rulare joc
 
 
@@ -6,23 +6,20 @@ from useri import user_dict
     
 
 def joc():
-    first = True
-    if first == True:
-        rand3 = ["[_]", "[_]", "[_]"]
-        rand2 = ["[_]", "[_]", "[_]"]
-        rand1 = ["[_]", "[_]", "[_]"]
-        tabla = [rand1, rand2, rand3]
-        print(f"3 > {rand3}\n2 > {rand2}\n1 > {rand1}\n       ^      ^      ^\n       A      B      C")
-    first = False
-
-    user1 = input("Introdu coordonatele(Se incepe cu randurile, respectiv 1, 2 sau 3 si apoi coloanele - A, B sau C >>> ").lower()
-    if user1[1] == "a":
+    rand3 = ["[_]", "[_]", "[_]"]
+    rand2 = ["[_]", "[_]", "[_]"]
+    rand1 = ["[_]", "[_]", "[_]"]
+    tabla = [rand1, rand2, rand3]
+    print(f"3 > {rand3}\n2 > {rand2}\n1 > {rand1}\n       ^      ^      ^\n       A      B      C")
+    
+    xul = input(f"{useri_dict['user_X'][0]} introdu' coordonatele (Se incepe cu randurile, respectiv 1, 2 sau 3 si apoi coloanele - A, B sau C >>> ").lower()
+    if xul[1] == "a":
         sec_digit = 0
-    elif user1[1] == "b":
+    elif xul[1] == "b":
         sec_digit = 1
-    elif user1[1] == "c":
+    elif xul[1] == "c":
         sec_digit = 2
-    first_digit = user1[0]
+    first_digit = xul[0]
     first_digit = int(first_digit)
 
     tabla = tabla[first_digit - 1]
@@ -30,5 +27,11 @@ def joc():
     tabla[sec_digit] = "[x]"
     print(f"3 > {rand3}\n2 > {rand2}\n1 > {rand1}\n       ^      ^      ^\n       A      B      C")
 
-if len(user_dict) == 2:
+
+
+
+
+
+
+if len(useri_dict) == 2:
     joc()
