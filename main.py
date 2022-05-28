4d1ccabc4eedcfcd90f63ea5647ad63a525f78da
cadfrunze <78301491+cadfrunze@users.noreply.github.com>
2022-05-28 16:44:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from useri import *
 from jocul import *
 
+
+
 final_game = True
 raspuns = ""
 while final_game:
     if start_signup == True:
         joc(tabla,mark_x,mark_o, prim_x, prim_o, empty_box, useri_dict, userul_x, userul_o)
-        raspuns = input("""1. Doresti sa joci in continuare?. Scrie \"1\" >>> 
-        2. Doresti sa innregistrezi playeri noi?. Scrie \"2\" >>> 
-        3. Doresti sa iesi din joc?. Scrie \"da\", \"3\" sau \"x\" >>> """)
+        castigatori(useri_dict, userul_x, userul_o)
+        print("1. Doresti sa joci in continuare?. Scrie \"1\" >>> \n2. Doresti sa innregistrezi playeri noi?. Scrie \"2\" >>> \n3. Doresti sa iesi din joc?. Scrie \"da\", \"3\" sau \"x\" >>> ")
+        raspuns = input("Raspunde aici >>> ")
