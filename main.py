2117f8bf0901a1a51627ac12b6961470ec67f181
cadfrunze <78301491+cadfrunze@users.noreply.github.com>
2022-05-28 17:05:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,5 +9,5 @@ while final_game:
     if start_signup == True:
         joc(tabla,mark_x,mark_o, prim_x, prim_o, empty_box, useri_dict, userul_x, userul_o)
         castigatori(useri_dict, userul_x, userul_o)
-        print("1. Doresti sa joci in continuare?. Scrie \"1\" >>> \n2. Doresti sa innregistrezi playeri noi?. Scrie \"2\" >>> \n3. Doresti sa iesi din joc?. Scrie \"da\", \"3\" sau \"x\" >>> ")
+        print("1. Doresti sa joci in continuare?. Scrie \"1\" >>> \n2. Doresti sa resetezi jocul si sa innregistrezi playeri noi?. Scrie \"2\" >>> \n3. Doresti sa iesi din joc?. Scrie \"da\", \"3\" sau \"x\" >>> ")
         raspuns = input("Raspunde aici >>> ")
