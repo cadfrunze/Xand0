071fbf50ada08d41874568654e6640fd6ce7f99d
cadfrunze <78301491+cadfrunze@users.noreply.github.com>
2022-05-28 10:08:31 +0300

    Update test3.py

--- test3.py
+++ test3.py
@@ -1,16 +1,6 @@
-rand3 = ["[_]", "[_]", "[_]"]
-rand2 = ["[_]", "[_]", "[_]"]
-rand1 = ["[_]", "[_]", "[_]"]
-col_a = [rand1[0], rand2[0], rand3[0]]
-col_b = [rand1[1], rand2[1], rand3[1]]
-col_c = [rand1[2], rand2[2], rand3[2]]
-oblic_1 = [rand1[0], rand2[1], rand3[2]]
-oblic_2 = [rand3[0], rand2[1], rand1[2]]
-tabla = [rand1, rand2, rand3, col_a, col_b, col_c, oblic_1, oblic_2]
-
-
-if rand2 == col_b:
-    print("ddssdsd")
-
+c = "maryus"
+a = f"{c}wewefffwfwwf"
+b = a.center(50, '*')
+print(b)
 
 
