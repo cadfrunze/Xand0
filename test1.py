4a9260da3a0adc2885b5bf22473a40a1cf332dec
cadfrunze <78301491+cadfrunze@users.noreply.github.com>
2022-05-29 16:31:08 +0300

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,158 +1,148 @@
 
-import random
-import os
-import time
 
-user1 = ""
-user2 = ""
-lista_useri = []
-# Innregistrare useri
-def useri():
-    necenzurat = ["Pula", "Pulla", "Pizda", "Puta", "Coi", "Coaie", "Sugaci", "Sugi", "Suji", "Tampit", "Fuck", "Laba",]
-    semne = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "`", "-", "=", "\"", ";", "'", ",", ".",
-            "~", "@", "#", "$", "%", "^", "&" ,"*", "(", ")", "_", "+", "[", "]", "{", "}", "|", ":", "<", ">", "?", "/", " "]
-    user_proba = ""
-    incercari = 0
-    game_1a = False
-    game_1b = False
-    game1 = True
-    while incercari <= 3 and game1 == True:
-        if incercari == 0:
-            user1 = input("Primul jucator, introdu' un nume >> ").capitalize()
-        for a in user1:
-            if a in semne:
-                continue
-            else:
-                user_proba = user_proba + a
-        if user_proba in necenzurat:
-            if incercari < 2:
-                os.system('cls')
-                user_proba = ""
-                user1 = input("Hey!, vezi ca ai scris o injuratura, scrieti un nume si gata! >>> ").capitalize()
-                incercari = incercari + 1
-                continue
-            elif incercari == 2:
-                os.system('cls')
-                user_proba = ""
-                user1 = input("Hey! ultima incercare! , ai scris o injuratura, scrieti un nume si gata! >>> ").capitalize()
-                incercari = incercari + 1
-                continue
-            elif incercari == 3:
-                os.system('cls')
-                print("Program inchis! Nerespectare cerinte")
-                game1 = False
-        elif len(user1) <= 2:
-            user_proba = ""
-            if incercari < 2:
-                os.system('cls')
-                user1 = input("Hey!, vezi numele tau contine mai putin de 3 caractere, scrieun nume cu un numar mai mare de 3 caractere! >>> ").capitalize()
-                incercari = incercari + 1
-                continue
-            elif incercari == 2:
-                os.system('cls')
-                user1 = input("Hey! ultima incercare!, vezi numele tau contine mai putin de 3 caractere, scrieun nume cu un numar mai mare de 3 caractere! >>> ").capitalize()
-                incercari = incercari + 1
-                continue
-            elif incercari == 3:
-                os.system('cls')
-                print("Program inchis! Nerespectare cerinte")
-                game1 = False
-        elif len(user1) >= 3 and user1 not in necenzurat:
-            print(f"Bun venit {user1}")
-            lista_useri.append(user1)
-            user_proba = ""
-            incercari = 0
-            game1 = False
-            game_1a = True
-    if game_1a == True:
-        game2 = True 
-        while incercari <= 3 and game2 == True:
-            if incercari == 0:
-                user2 = input("Al doilea jucator, introdu' un nume >> ").capitalize()
-            if user2 == user1:
-                os.system('cls')
-                if incercari == 2:
-                    user2 = input(f"Hey!, ultima incercare!, vezi ca ai introdus acelasi nume ca si {user1} >> ").capitalize()
-                    incercari = incercari + 1
-                    continue
-                elif incercari < 2:
-                    user2 = input(f"Hey!, vezi ca ai introdus acelasi nume ca si {user1} >> ").capitalize()
-                    incercari = incercari + 1
-                    continue
-                elif incercari == 3:
-                    print("Program inchis! Nerespectare cerinte")
-                    game2 = False
-            for a in user2:
-                if a in semne:
-                    continue
-                else:
-                    user_proba = user_proba + a
-            if user_proba in necenzurat:
-                if incercari < 2:
-                    os.system('cls')
-                    user_proba = ""
-                    user2 = input("Hmmm..., vezi ca ai scris o injuratura, scrieti un nume si gata! >>> ").capitalize()
-                    incercari = incercari + 1
-                    continue
-                elif incercari == 2:
-                    os.system('cls')
-                    user_proba = ""
-                    user2 = input("Hey! ultima incercare! , ai scris o injuratura, scrieti un nume si gata! >>> ").capitalize()
-                    incercari = incercari + 1
-                    continue
-                elif incercari == 3:
-                    os.system('cls')
-                    print("Program inchis! Nerespectare cerinte")
-                    game2 = False
-            elif len(user2) <= 2:
-                os.system('cls')
-                user_proba = ""
-                if incercari < 2:
-                    user2 = input("Hey!, vezi numele tau contine mai putin de 3 caractere, scrieun nume cu un numar mai mare de 3 caractere! >>> ").capitalize()
-                    incercari = incercari + 1
-                    continue
-                elif incercari == 2:
-                    user2 = input("Hey! ultima incercare!, vezi numele tau contine mai putin de 3 caractere, scrieun nume cu un numar mai mare de 3 caractere! >>> ").capitalize()
-                    incercari = incercari + 1
-                    continue
-                elif incercari == 3:
-                    print("Program inchis! Nerespectare cerinte")
-                    game2 = False
-            elif len(user2) >= 3 and user2 not in necenzurat and user2 != user1:
-                print(f"Bun venit si tie {user2}")
-                lista_useri.append(user2)
-                user_proba = ""
-                incercari = 0
-                game2 = False
-                game_1b = True
-    if game_1a == True and game_1b == True:       
-        useri_dict = {
-            "user_X": random.choice(lista_useri),
-            }
-        if useri_dict["user_X"] == user1:
-            useri_dict["user_X"] = [user1, "X", 0,]
-            useri_dict["user_0"] = [user2, "0", 0] 
-        elif useri_dict["user_X"] == user2:
-            useri_dict["user_X"] = [user2, "X", 0]
-            useri_dict["user_0"] = [user1, "0", 0]
-        return useri_dict
-os.system('cls')
+# def tabla():
+#     rand3 = ["[_]", "[_]", "[_]"]
+#     rand2 = ["[_]", "[_]", "[_]"]
+#     rand1 = ["[_]", "[_]", "[_]"]
+#     tabla = [rand1, rand2, rand3,]
+#     tabla = f"3 > {rand3}\n2 > {rand2}\n1 > {rand1}\n       ^      ^      ^\n       A      B      C"
+#     return tabla
+# def tabla_x():
+#     rand3_x = ["[x]", "[x]", "[x]"]
+#     rand2_x = ["[x]", "[x]", "[x]"]
+#     rand1_x = ["[x]", "[x]", "[x]"]
+#     tabla_x = [rand1_x, rand2_x, rand3_x,]
+#     return tabla_x
+# def tabla_o():
+#     rand3_o = ["[o]", "[o]", "[o]"]
+#     rand2_o = ["[o]", "[o]", "[o]"]
+#     rand1_o = ["[o]", "[o]", "[o]"]
+#     tabla_o = [rand1_o, rand2_o, rand3_o,]
+#     return tabla_o
+    
+# mark_x = "[x]"
+# mark_o = "[o]"
+# prim_x = [mark_x, mark_x, mark_x]
+# prim_o = [mark_o, mark_o, mark_o]
+# empty_box = "[_]"
+# list_incercari = []
 
-useri_dict = useri()
-print(f"Primul care incepe este {useri_dict['user_X'][0]} si joaca cu {useri_dict['user_X'][2]}, iar apoi {useri_dict['user_0'][0]} si joaca cu {useri_dict['user_0'][2]}")
-print(useri_dict)
+# userul_o = useri_dict['user_0'][0]
+# userul_o = userul_o.center(30, "*")
+# userul_x = useri_dict['user_X'][0]
+# userul_x = userul_x.center(30, "*")
 
+# # Functie marcare X
+# def push_x(tabla,mark_x, useri_dict, userul_x):
+#     """Jocul pt X"""
+#     x_u = useri_dict["user_X"][1]
+#     x_u = x_u.center(30, "*")
+#     tabel_nr = ["1", "2", "3"]
+#     tabel_lit = ["a", "b", "c"]
+#     game1 = True
+#     cuvant1 = True
+#     print(userul_x)
+#     print(x_u)
+#     while game1:
+#         if cuvant1 == True:
+#             xul = input(">>> ")
+#         if xul[0] == "" or xul[0] == "" or xul[0] not in tabel_nr:
+#             xul = input(f" Hey! {useri_dict['user_X'][0]}, vezi ca ai gresit! Introdu' coordonatele (Se incepe cu randurile, respectiv 1, 2 sau 3 si apoi coloanele - A, B sau C >>> ").lower()
+#             cuvant1 = False
+#             continue
+#         elif xul[1] == "" or xul[1] == " " or xul[1] not in tabel_lit:
+#             xul = input(f" Hey! {useri_dict['user_X'][0]}, vezi ca ai gresit! Introdu' coordonatele (Se incepe cu randurile, respectiv 1, 2 sau 3 si apoi coloanele - A, B sau C >>> ").lower()
+#             cuvant1 = False
+#             continue
+#         while xul in list_incercari:
+#             xul = input(f" Hey! {useri_dict['user_X'][0]}, vezi ca ai gresit! (coordonate) Introdu' coordonatele (Se incepe cu randurile, respectiv 1, 2 sau 3 si apoi coloanele - A, B sau C >>> ").lower()
+#         list_incercari.append(xul)
+#         cuvant1 = True
+#         first_digit = int(xul[0])
+#         if xul[1] == "a":
+#             sec_digit = 0
+#         elif xul[1] == "b":
+#             sec_digit = 1
+#         elif xul[1] == "c":
+#             sec_digit = 2
+#         tabla = tabla[first_digit - 1]
+#         tabla[sec_digit] = len(tabla) - sec_digit
+#         tabla[sec_digit] = mark_x
+#         game1 = False
 
+# # Functie marcare 0
+# def push_o(tabla, mark_o, useri_dict, userul_o):
+#     """Jocul pt 0"""
+#     o_u = useri_dict["user_0"][1]
+#     o_u = o_u.center(30, "*")
+#     tabel_nr = ["1", "2", "3"]
+#     tabel_lit = ["a", "b", "c"]
+#     game1 = True
+#     cuvant1 = True
+#     print(userul_o)
+#     print(o_u)
+#     while game1:
+#         if cuvant1 == True:
+#             oul = input(">>> ")
+#         if oul[0] == "" or oul[0] == " " or oul[0] not in tabel_nr:
+#             oul = input(f" 0 Hey! {useri_dict['user_0'][0]}, vezi ca ai gresit! Introdu' coordonatele (Se incepe cu randurile, respectiv 1, 2 sau 3 si apoi coloanele - A, B sau C >>> ")
+#             cuvant1 = False
+#             continue
+#         elif oul[1] == "" or oul[1] == " " or oul[1] not in tabel_lit:
+#             oul = input(f" 0 Hey! {useri_dict['user_0'][0]}, vezi ca ai gresit! Introdu' coordonatele (Se incepe cu randurile, respectiv 1, 2 sau 3 si apoi coloanele - A, B sau C >>> ")
+#             cuvant1 = False
+#             continue
+#         while oul in list_incercari:
+#             oul = input(f"0  Hey! {useri_dict['user_0'][0]}, vezi ca ai gresit (coordonate)! Introdu' coordonatele (Se incepe cu randurile, respectiv 1, 2 sau 3 si apoi coloanele - A, B sau C >>> ").lower()
+#         list_incercari.append(oul)
+#         cuvant1 = True
+#         first_digit = int(oul[0])
+#         if oul[1] == "a":
+#             sec_digit = 0
+#         elif oul[1] == "b":
+#             sec_digit = 1
+#         elif oul[1] == "c":
+#             sec_digit = 2
+#         tabla = tabla[first_digit - 1]
+#         tabla[sec_digit] = len(tabla) - sec_digit
+#         tabla[sec_digit] = mark_o
+#         game1 = False
+            
 
+# # Functie jocul....Greu ii futa-l drequ'
+# def joc(tabla,mark_x,mark_o, prim_x, prim_o, empty_box, useri_dict, userul_x, userul_o):
+#     """Castigator sau remiza....Vai futa-l drequ'"""
+#     incercari_x = 0
+#     incercari_o = 0
+#     prima_etapa = True
+   
 
-
-
-
-
-
-
-
-
-
+# # Afisare castigator sau remiza
+# def castigatori(useri_dict, userul_x, userul_o):
+#     os.system('cls')
+#     print(f"3 > {rand3}\n2 > {rand2}\n1 > {rand1}\n       ^      ^      ^\n       A      B      C")
+#     if useri_dict["user_X"][2] > useri_dict["user_0"][2]:
+#         if useri_dict["user_X"][2] == 1:
+#             print(userul_x)
+#             print(useri_dict["user_X"][0] + " " + "a castigat! si are " + str(useri_dict["user_X"][2]) + " " + "punct")
+#         else:
+#             print(userul_x)
+#             print(useri_dict["user_X"][0] + " " + "a castigat! si are " + str(useri_dict["user_X"][2]) + " " + "puncte")
+#     elif useri_dict["user_0"][2] > useri_dict["user_X"][2]:
+#         if useri_dict["user_0"][2] == 1:
+#             print(userul_o)
+#             print(useri_dict["user_0"][0] + " " + "a castigat! si are " + str(useri_dict["user_0"][2]) + " " + "punct")
+#         else:
+#             print(userul_o)
+#             print(useri_dict["user_0"][0] + " " + "a castigat! si are " + str(useri_dict["user_0"][2]) + " " + "puncte")
+#     else:
+#         remiza = "Remiza"
+#         remiza = remiza.center(30, "*")
+#         print(remiza)
+#         print("scor: " + str(useri_dict["user_X"][2]) + " >>> " + useri_dict["user_X"][0])
+#         print("scor: " + str(useri_dict["user_0"][2]) + " >>> " + useri_dict["user_0"][0])
+        
+        
+    
 
 
