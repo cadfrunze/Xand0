0debacc902fe174a5706a5c13eff5439ab88de0c
cadfrunze <78301491+cadfrunze@users.noreply.github.com>
2022-05-29 16:31:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,20 +10,12 @@ while final_game == True:
     if start_signup == True and raspuns == "":
         joc(tabla,mark_x,mark_o, prim_x, prim_o, empty_box, useri_dict, userul_x, userul_o)
         castigatori(useri_dict, userul_x, userul_o)
-        rand3 = ["[_]", "[_]", "[_]"]
-        rand2 = ["[_]", "[_]", "[_]"]
-        rand1 = ["[_]", "[_]", "[_]"]
-        tabla = [rand1, rand2, rand3,]
-        mark_x = "[x]"
-        mark_o = "[o]"
-        prim_x = [mark_x, mark_x, mark_x]
-        prim_o = [mark_o, mark_o, mark_o]
-        empty_box = "[_]"
-        list_incercari = []
         print("1. Doresti sa joci in continuare?. Scrie \"1\" >>> \n2. Doresti sa resetezi jocul si sa innregistrezi playeri noi?. Scrie \"2\" >>> \n3. Doresti sa iesi din joc?. Scrie \"da\", \"3\" sau \"x\" >>> ")
         raspuns = input("Raspunde aici >>> ")
     elif raspuns == "1":
         start_signup = False
+        useri_dict["user_X"][3] = []
+        useri_dict["user_0"][3] = []
         joc(tabla,mark_x,mark_o, prim_x, prim_o, empty_box, useri_dict, userul_x, userul_o)
         castigatori(useri_dict, userul_x, userul_o)
         print("1. Doresti sa joci in continuare?. Scrie \"1\" >>> \n2. Doresti sa resetezi jocul si sa innregistrezi playeri noi?. Scrie \"2\" >>> \n3. Doresti sa iesi din joc?. Scrie \"da\", \"3\" sau \"x\" >>> ")
