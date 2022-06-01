b1400f8626a91345a63203c3b7b4689ef562e769
cadfrunze <78301491+cadfrunze@users.noreply.github.com>
2022-06-01 19:56:15 +0300

    Update jocul.py

--- jocul.py
+++ jocul.py
@@ -374,7 +374,7 @@ while final_game == True:
     if start_signup == True and raspuns == "":
         joc(tabla,mark_x,mark_o, prim_x, prim_o, empty_box, useri_dict, userul_x, userul_o)
         # castigatori(useri_dict, userul_x, userul_o)
-        print("1. Doresti sa joci in continuare?. Scrie \"1\" >>> \n2. Doresti sa resetezi jocul si sa innregistrezi playeri noi?. Scrie \"2\" >>> \n3. Doresti sa iesi din joc?. Scrie \"da\", \"3\" sau \"x\" >>> ")
+        print("1. Doresti sa joci in continuare?. Scrie \"1\" >>> \n2. Doresti sa resetezi scorul?. Scrie \"2\" >>> \n3. Doresti sa resetezi si sa innregistrezi playeri noi?\n4. Doresti sa iesi din joc?. Scrie \"da\", \"3\" sau \"x\" >>> ")
         raspuns = input("Raspunde aici >>> ")
     elif raspuns == "1":
         rand3 = ["[_]", "[_]", "[_]"]
