81b4e2cc8f0928f12a417498f22c91b9703b9e2f
cadfrunze <78301491+cadfrunze@users.noreply.github.com>
2022-06-03 09:01:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,22 +2,23 @@
 
 
 
-
 # final_game = True
 # raspuns = ""
 # while final_game == True:
-#     if start_signup == True and raspuns == "":
+#     if start_signup == True:
 #         joc(tabla,mark_x,mark_o, prim_x, prim_o, empty_box, useri_dict, userul_x, userul_o)
-#         castigatori(useri_dict, userul_x, userul_o)
-#         print("1. Doresti sa joci in continuare?. Scrie \"1\" >>> \n2. Doresti sa resetezi jocul si sa innregistrezi playeri noi?. Scrie \"2\" >>> \n3. Doresti sa iesi din joc?. Scrie \"da\", \"3\" sau \"x\" >>> ")
+#         print("1. Doresti sa joci in continuare?. Scrie \"1\" >>> \n2. Doresti sa resetezi scorul?. Scrie \"2\" >>> \n3. Doresti sa resetezi si sa innregistrezi playeri noi? >>> \n4. Doresti sa iesi din joc?. Scrie \"da\", \"3\" sau \"x\" >>> ")
 #         raspuns = input("Raspunde aici >>> ")
 #     elif raspuns == "1":
+#         rand3 = ["[_]", "[_]", "[_]"]
+#         rand2 = ["[_]", "[_]", "[_]"]
+#         rand1 = ["[_]", "[_]", "[_]"]
+#         alta_tabla = [rand1, rand2, rand3,]
+#         alta_tabla = tabla
 #         start_signup = False
 #         useri_dict["user_X"][3] = []
 #         useri_dict["user_0"][3] = []
 #         joc(tabla,mark_x,mark_o, prim_x, prim_o, empty_box, useri_dict, userul_x, userul_o)
-#         castigatori(useri_dict, userul_x, userul_o)
 #         print("1. Doresti sa joci in continuare?. Scrie \"1\" >>> \n2. Doresti sa resetezi jocul si sa innregistrezi playeri noi?. Scrie \"2\" >>> \n3. Doresti sa iesi din joc?. Scrie \"da\", \"3\" sau \"x\" >>> ")
 #         raspuns = input("Raspunde aici >>> ")
-
-         
+        
